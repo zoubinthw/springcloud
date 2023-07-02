e73e885bf8127234b0d48e42c8d4529b45e9dbf0
zoubinthw <316324334@qq.com>
2023-07-02 21:07:32 +0800

    some error

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/CloudProviderPayment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/CloudProviderPayment8001Application.java
@@ -1,4 +1,4 @@
-package com.atguigu.springcloud.payment8001;
+package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
