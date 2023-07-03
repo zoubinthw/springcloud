c212e6629425a04e4a16086ffd18599847929b68
zoubinthw <316324334@qq.com>
2023-07-03 17:03:28 +0800

    [p13][binzou][]为idea项目配置ServiceDashboard

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/CloudProviderPayment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/CloudProviderPayment8001Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudProviderPayment8001Application {
+public class CloudProviderPayment8001Application { // P13
 
     public static void main(String[] args) {
         SpringApplication.run(CloudProviderPayment8001Application.class, args);
