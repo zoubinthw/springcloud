0fdc0fae377ff9378a7e9b40f0115978098b2657
zoubinthw <316324334@qq.com>
2023-07-03 16:51:14 +0800

    重新初始化

---
+++ cloud-consumer-order8002/src/main/java/com/atguigu/springcloud/consumerorder8002/CloudConsumerOrder8002Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud.consumerorder8002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConsumerOrder8002Application { // P12
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConsumerOrder8002Application.class, args);
+    }
+
+}
