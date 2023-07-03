dcd8f697d04c6c847fe6a0db2a44a9ad72e6d10c
zoubinthw <316324334@qq.com>
2023-07-03 21:39:18 +0800

    [p21][binzou][feat]配置provider集群，这里2点，1：consumer不再以直接访问特定终端，而是访问服务的名称；2：consumer的restTemplate要开启负载均衡，不然不知道使用哪台主机

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -12,16 +13,18 @@ import javax.annotation.Resource;
 @Slf4j
 public class PaymentController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @Resource
     private PaymentService paymentService; //
-
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("-------插入的结果: " + result);
         if (result > 0) {
-            return new CommonResult(200, "插入数据成功", result);
+            return new CommonResult(200, "插入数据成功, serverPort: " + serverPort + "," + result);
         } else {
             return new CommonResult(404, "插入数据失败", null);
         }
@@ -30,9 +33,9 @@ public class PaymentController {
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
-        log.info("-------插入结果: " + payment);
+        log.info("-------查询结果: " + payment);
         if (payment != null) {
-            return new CommonResult(200, "查询数据成功", payment);
+            return new CommonResult(200, "查询数据成功, serverPort: " + serverPort + "," + payment);
         } else {
             return new CommonResult(404, "没有对应记录，查询ID: " + id, null);
         }
