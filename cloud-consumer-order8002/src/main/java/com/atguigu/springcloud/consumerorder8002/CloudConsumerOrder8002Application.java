c86d38f388b5539f98e1a660a5d7c060eac6797d
zoubinthw <316324334@qq.com>
2023-07-03 16:52:33 +0800

    重新初始化8002

--- cloud-consumer-order8002/src/main/java/com/atguigu/springcloud/consumerorder8002/CloudConsumerOrder8002Application.java
+++ cloud-consumer-order8002/src/main/java/com/atguigu/springcloud/consumerorder8002/CloudConsumerOrder8002Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudConsumerOrder8002Application { // P12
+public class CloudConsumerOrder8002Application {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudConsumerOrder8002Application.class, args);
