bb3fb02eac3ed41fcb1ec35c02078e01fac493f5
zoubinthw <316324334@qq.com>
2023-07-03 19:07:37 +0800

    [p18][binzou][feat]将order和payment服务注册到Eureka中心

--- cloud-consumer-order8002/src/main/java/com/atguigu/springcloud/consumerorder8002/CloudConsumerOrder8002Application.java
+++ cloud-consumer-order8002/src/main/java/com/atguigu/springcloud/consumerorder8002/CloudConsumerOrder8002Application.java
@@ -2,8 +2,10 @@ package com.atguigu.springcloud.consumerorder8002;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudConsumerOrder8002Application {
 
     public static void main(String[] args) {
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/CloudProviderPayment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/CloudProviderPayment8001Application.java
@@ -2,8 +2,10 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudProviderPayment8001Application { // P13
 
     public static void main(String[] args) {
