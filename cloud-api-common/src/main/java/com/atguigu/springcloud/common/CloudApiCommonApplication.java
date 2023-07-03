64ad9fc1ba907f553f5b8c2a5545e5c804a3772a
zoubinthw <316324334@qq.com>
2023-07-03 17:33:07 +0800

    [12][binzou][refactor]: 添加用于重构的通用模块

---
+++ cloud-api-common/src/main/java/com/atguigu/springcloud/common/CloudApiCommonApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud.common;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+public class CloudApiCommonApplication {
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(CloudApiCommonApplication.class, args);
+//	}
+
+}
