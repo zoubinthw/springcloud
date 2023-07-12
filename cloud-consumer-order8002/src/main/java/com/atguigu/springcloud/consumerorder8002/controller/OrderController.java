bb6214ced6c44100b35b6aa9e69d880349c6c154
zoubinthw <316324334@qq.com>
2023-07-12 23:11:43 +0800

    [p37][binzou][feat]entity和object的区别

--- cloud-consumer-order8002/src/main/java/com/atguigu/springcloud/consumerorder8002/controller/OrderController.java
+++ cloud-consumer-order8002/src/main/java/com/atguigu/springcloud/consumerorder8002/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.consumerorder8002.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,12 +24,23 @@ public class OrderController {
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
         log.info("创建对象");
-        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+//        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        return restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class).getBody();
     }
 
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         log.info("获取对象");
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> resultEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (resultEntity.getStatusCode().is2xxSuccessful()) {
+            return resultEntity.getBody();
+        } else {
+            return new CommonResult(400, "操作失败");
+        }
     }
 }
