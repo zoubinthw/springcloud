6a8ebd3d2efc6ff8a7e55ff9afac3102d16b0d24
zoubinthw <316324334@qq.com>
2023-07-13 11:18:06 +0800

    [p41][binzou][feat]配置负载均衡算法,注意这里负载均衡器不能放在@ComponentScan包下或其子包下,否则就达不到定制化的效果了，因为全部请求都会走这个负载算法

--- cloud-consumer-order8002/src/main/java/com/atguigu/springcloud/consumerorder8002/CloudConsumerOrder8002Application.java
+++ cloud-consumer-order8002/src/main/java/com/atguigu/springcloud/consumerorder8002/CloudConsumerOrder8002Application.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class)
 public class CloudConsumerOrder8002Application {
 
     public static void main(String[] args) {
