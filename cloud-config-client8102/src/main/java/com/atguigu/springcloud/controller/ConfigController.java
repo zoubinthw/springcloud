cdaa0c7d9c732048783a550b43faed84ff275a08
zoubinthw <316324334@qq.com>
2023-07-23 00:15:40 +0800

    ***

---
+++ cloud-config-client8102/src/main/java/com/atguigu/springcloud/controller/ConfigController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RefreshScope
+public class ConfigController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/configInfo")
+    public String configInfo()
+    {
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+}
