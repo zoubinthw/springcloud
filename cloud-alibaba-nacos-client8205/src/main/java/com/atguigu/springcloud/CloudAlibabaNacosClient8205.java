08190bf52a6c784909c53bec9a75e5a4d752d5e7
zoubinthw <316324334@qq.com>
2023-07-25 17:35:12 +0800

    [p101][binzou][feat][记得删除config的git密码]nacos配置中心: 注意在低版本的nacos, data-id的后缀名只能是yaml

---
+++ cloud-alibaba-nacos-client8205/src/main/java/com/atguigu/springcloud/CloudAlibabaNacosClient8205.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudAlibabaNacosClient8205 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAlibabaNacosClient8205.class, args);
+    }
+}
