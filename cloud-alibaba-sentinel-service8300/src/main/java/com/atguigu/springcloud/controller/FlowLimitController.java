ae64d4e03e14200c1ed5fe37d5a63bb9f9821435
zoubinthw <316324334@qq.com>
2023-07-27 17:08:00 +0800

    [p121-123][binzou][feat][记得删除config的git密码]sentinel关于热点key的限流规则

--- cloud-alibaba-sentinel-service8300/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8300/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -1,8 +1,11 @@
 package com.atguigu.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.atguigu.springcloud.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -48,5 +51,18 @@ public class FlowLimitController {
         log.info("testD 测试异常数目");
         int age = 10/0;
         return "------testD";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealTestHotKey") // 这里的value可以是任何字符串，只要名称唯一即可
+    public String testHotKey(
+            @RequestParam(value = "p1", required = false) String p1,
+            @RequestParam(value = "p2", required = false) String p2
+    )
+    {
+        return "------testHotKey p1: " + p1 + ", p2: " + p2;
+    }
+    public String dealTestHotKey(String p1, String p2, BlockException exception) {
+        return "---------dealTestHotKey, ~~~~~~~~~";
     }
 }
