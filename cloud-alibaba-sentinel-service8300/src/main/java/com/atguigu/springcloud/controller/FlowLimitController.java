03afc6d36bb83137d376adce3dbadf1da029ffa4
zoubinthw <316324334@qq.com>
2023-07-27 16:32:57 +0800

    [p121-123][binzou][feat][记得删除config的git密码]sentinel熔断: RT, 异常比例, 异常数目

--- cloud-alibaba-sentinel-service8300/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8300/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -25,5 +26,27 @@ public class FlowLimitController {
     {
 //        return testService.common();
         return "testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD()
+    {
+        //暂停几秒钟线程
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+//        log.info("testD 测试RT");
+
+        // 异常比例&数目测试
+        log.info("testD 测试异常占比");
+        int age = 10/0;
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE()
+    {
+        // 异常数目测试
+        log.info("testD 测试异常数目");
+        int age = 10/0;
+        return "------testD";
     }
 }
