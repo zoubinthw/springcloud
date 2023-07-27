2dd8ef0833a6666fabdcfdfdc42e8e51ad61d4cf
zoubinthw <316324334@qq.com>
2023-07-27 17:40:59 +0800

    [127][binzou][feat][记得删除config的git密码]sentinel自定义限流信息

---
+++ cloud-alibaba-sentinel-service8300/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RateLimitController {
+    @GetMapping("/byResource")
+    @SentinelResource(value = "byResource",blockHandler = "handleException")
+    public CommonResult byResource()
+    {
+        return new CommonResult(200,"按资源名称限流测试OK",new Payment(2020L,"serial001"));
+    }
+    public CommonResult handleException(BlockException exception)
+    {
+        return new CommonResult(444,exception.getClass().getCanonicalName()+"\t 服务不可用");
+    }
+
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl", blockHandler = "handleException") // 如果用url进行限流，这里配不配blockHandler都会使用系统默认的handle, 所以还是建议用SentinelResource的资源限流，可以使用自定义处理方法
+    public CommonResult byUrl()
+    {
+        return new CommonResult(200,"按url限流测试OK",new Payment(2020L,"serial002"));
+    }
+}
